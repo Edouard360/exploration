b1bebec06a475a7c1e7f987f1246561523c71ad3
Edouard360 <mailedouardmehlman@gmail.com>
2017-06-08 15:17:15 +0200

    Adding Plot object in insight and correcting attributes in main Observation class

--- insight.py
+++ insight.py
@@ -6,7 +6,20 @@ insight.py
 """
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.dates import HourLocator, DateFormatter
+
 from insight_tools import check_type, plot
+import pandas as pd
+
+deb1 = ["DEB1-1","DEB1-2","DEB1-3","DEB1-4"]
+deb2 = ["DEB2-1","DEB2-2","DEB2-3","DEB2-4"]
+deb3 = ["DEB3-1","DEB3-2","DEB3-3","DEB3-4"]
+tmp =  ["TEM3-1","TEM3-2","TEM3-3","TEM3-4"]
+tmp2 = ["TEM2-"]# Température fuite joint 1
+tmp3 = ["TEM1-"] # Température ligne d'injection aux joints (en * Celsius)
+deb35 = ["DEB3-5"]
+pre = ["PRE-"]
+pui = ["PUI-"]
 
 class Insight:
 
@@ -46,3 +59,58 @@ class Insight:
         for i,obs in enumerate(observations):
             ax = axes[i]
             plot(obs.df,ax = ax,xlim = xlim);ax.legend().remove()
+
+
+class Plot:
+    def __init__(self,list_df):
+        self.list_df = list_df
+
+    def plot_all(self,tight_boolean, i):
+        if (type(self.list_df) is pd.DataFrame):
+            df = self.list_df
+        else:
+            df = self.list_df[i]
+        # plt.figure(figsize=(width, height))
+        print("From : " + str(df.index[0].strftime("%d/%m/%Y"))
+              + "\nTo   : " + str(df.index[-1].strftime("%d/%m/%Y")))
+        if not tight_boolean:
+            width = 10
+            height = 20
+            fig, axes = plt.subplots(nrows=8, ncols=1, figsize=(width, height), sharex=True)
+        else:
+            width = 12
+            height = 12
+            fig, axes = plt.subplots(nrows=4, ncols=2, figsize=(width, height), sharex=True)
+            axes = [axes[int(i % 4), int(i / 4)] for i in range(8)]
+        [d1, d2, d3, d4] = axes[0].plot(df[deb1])
+        axes[0].legend([d1, d2, d3, d4], deb1, loc='best')
+        axes[0].set_title("Débit de fuite au joint 1 (Gamme Large)")
+        axes[1].plot(df[deb2])
+        axes[1].set_title("Débit de fuite au joint 1 (Gamme Étroite)")
+        axes[2].plot(df[tmp])
+        axes[2].set_title("Température eau joint 1 - 051PO")
+
+        axes[3].plot(df[tmp2])
+        axes[3].set_title("Température fuites joint 1")
+
+        axes[4].plot(df[deb3])
+        axes[4].set_title("Débit d'injection au joint")
+        axes[4].set_facecolor("#d5e5e5")
+        axes[5].plot(df[tmp3])
+        axes[5].set_title("Température ligne d'injection aux joints (en * Celsius)")
+        axes[5].set_facecolor("#d5e5e5")
+
+        axes[6].plot(df[pre], 'b')
+        axes[6].set_title("Pression (BAR)")
+        axes[6].set_facecolor("#d1d1d1")
+        axes[7].plot(df[pui], 'k')
+        axes[7].set_title("Puissance Nominale (%)")
+        axes[7].set_facecolor("#d1d1d1")
+
+        axes[0].get_xaxis().set_ticks([])
+        hour_locator = HourLocator([0, 12])
+        axes[0].xaxis.set_major_locator(hour_locator)
+        axes[0].xaxis.set_major_formatter(DateFormatter("%H:%M"))
+
+        plt.tight_layout()
+        plt.show()
--- observation.py
+++ observation.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
-from datetime import timedelta
+from datetime import timedelta,datetime
 from tools import low_pass_filter, bandstop_filter, sequence_to_interval, lazyprop
 from insight_tools import plot
 from rolling import Rolling
 from interval import Interval
+import dateutil.parser
 
 class Observation:
-    def __init__(self, path, files_name, files_tag, format="%d-%b-%y %H:%M:%S.0", ncol=4):
+    def __init__(self, path, files_name, files_tag, format="%d-%b-%y %H:%M:%S.0", ncol=4,hours_bad_level=15,hours_interval=0):
         print("Loading in memory %i observations..." % (int(len(files_name))))
         list_df = [pd.read_csv(path + file_name, sep=";") for file_name in files_name]
         self.ncol = ncol
@@ -26,9 +27,12 @@ class Observation:
         if self.ncol == 4:
             self.df = df.ix[:, ["value" in column for column in df.columns]]
             self.level_df = df.ix[:, ["level" in column for column in df.columns]]
+        self.hours_bad_level = hours_bad_level
+        self.hours_interval = hours_interval # The duration of each interval ! 0 if the max value possible is desired
+        self.hours_low_sampling = 1
 
-    def split_valid_intervals_df(self, hours=50):
-        return self.get_valid_interval(hours=hours).split_accordingly(self.df)
+    def split_valid_intervals_df(self, hours=0, hours_bad_level=1):
+        return self.get_valid_interval().split_accordingly(self.df)
 
     def split_healthy_unhealthy(self,healthy=(0.1, 0.3),unhealthy=(0.7, 0.9)):
         healthy_intervals, unhealthy_intervals = self.longest_valid_intervals.separate_intervals(healthy,unhealthy)
@@ -36,13 +40,23 @@ class Observation:
         unhealthy_ts = Interval(unhealthy_intervals).split_accordingly(self.df)
         return healthy_ts, unhealthy_ts
 
-    def get_valid_interval(self, hours=50):
-        valid_intervals_to_explore = self.intervals_to_remove.valid_ante_interval(self.start_bad_level, hours=hours)
+    def get_valid_interval(self):
+        valid_intervals_to_explore = self.intervals_to_remove.valid_ante_interval(self.start_bad_level, hours=self.hours_interval)
         return Interval(valid_intervals_to_explore)
+
+    def get_interval_id(self,date):
+        if(type(date) is not datetime):
+            if(type(date) is str):
+                date = dateutil.parser.parse(date)
+        return np.sum(self.longest_valid_intervals.intervals[:, 0] < date)
+
+    @lazyprop
+    def full_concatenated_df(self):
+        return pd.concat(self.split_valid_intervals_df(),axis=0)
 
     @lazyprop
     def longest_valid_intervals(self):
-        return self.get_valid_interval(hours=0)
+        return self.get_valid_interval()
 
     @lazyprop
     def intervals_to_remove(self):
@@ -52,10 +66,10 @@ class Observation:
         return interval_to_remove
 
     @lazyprop
-    def intervals_low_sample(self, hours=1):
+    def intervals_low_sample(self):
         print("Analysing intervals with low sampling rate")
         time_btw = self.df.index[1:] - self.df.index[:-1]
-        ind_tmp = time_btw > timedelta(hours=hours)
+        ind_tmp = time_btw > timedelta(hours=self.hours_low_sampling)
         start_ind = np.concatenate((ind_tmp, [False]))
         end_ind = np.concatenate(([False], ind_tmp))
         return np.array(list(zip(self.df.index[start_ind], self.df.index[end_ind])))
@@ -76,7 +90,7 @@ class Observation:
             bad_timestamp = sorted(bad_timestamp_label.tolist() + bad_timestamp_value.tolist())
         else:
             bad_timestamp = sorted(bad_timestamp_value.tolist())
-        bad_intervals = sequence_to_interval(bad_timestamp, timedelta(hours=15))
+        bad_intervals = sequence_to_interval(bad_timestamp, timedelta(hours=self.hours_bad_level))
         return bad_intervals
 
     @lazyprop
